af3684ba1ac245f6fa71e7dd7a8d93886dd845ec
kwons0 <86402215+kwons0@users.noreply.github.com>
2021-08-10 13:10:34 +0900

    pizza

--- js/index.js
+++ js/index.js
@@ -5,23 +5,55 @@ function main(){
 
     function callback(data){
 
-        // 헤더 서브메뉴
-        const menuBtn = document.querySelector('.my a:nth-of-type(3)'),
-        subMenu = document.querySelector('.submenu > div'),
-        subBg = document.querySelector('.submenu > p');
+        // 헤더 버거메뉴
+        const menuBtn = document.querySelector('.my a:nth-of-type(3)'), //버거버튼
+        subMenu = document.querySelector('.submenu > div'), //서브 내용
+        subWhite = document.querySelector('.submenu > p'), //서브 흰배경
+        subBlack = document.querySelector('header');
 
         menuBtn.addEventListener('click', function(){
             if(menuBtn.classList.contains('active')){
                 menuBtn.classList.remove('active');
                 subMenu.classList.remove('active');
-                subBg.classList.remove('active');
+                subWhite.classList.remove('active');
+                subBlack.classList.remove('black');
+
             }else{
                 menuBtn.classList.add('active');
                 subMenu.classList.add('active');
-                subBg.classList.add('active');
+                subWhite.classList.add('active');
+                subBlack.classList.add('black');
             }
             
         });
+
+
+        //헤더 미니메뉴
+        $('.menu ul:nth-of-type(1) li').on({
+            mouseenter:function(){
+                $(this).find('div').stop().slideDown(550);
+                if( $(this).children().length == 2){
+                    $('.backwhite').addClass('white');
+                }
+            },
+            mouseleave:function(){
+                $(this).find('div').stop().slideUp(150);
+                $('.backwhite').removeClass('white');
+            }
+        })
+
+
+        // $('.menu ul:nth-of-type(1) li:nth-of-type(1)').hover(
+        //     function(){
+        //         $('.mini li:nth-of-type(1)').stop().slideDown(500);
+        //         $('.backwhite').addClass('white');
+        //     },
+        //     function(){
+        //         $('.mini li:nth-of-type(1)').stop().slideUp(200);
+        //         $('.backwhite').removeClass('white');
+        //     }
+        // )
+
 
         // 피자 목록
         let tagList='';
