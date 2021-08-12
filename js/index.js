faf13120916c6f8156abfe5fc8eafad178c54f82
kwons0 <86402215+kwons0@users.noreply.github.com>
2021-08-12 19:09:53 +0900

    pizzahut

--- js/index.js
+++ js/index.js
@@ -5,6 +5,27 @@ function main(){
 
     function callback(data){
 
+        // 월쑤데이 팝업
+        const todayPop = document.querySelector('.popup'),
+            todayBtn = document.querySelector('.popup div button:nth-of-type(1)');
+
+        todayBtn.onclick = function(){
+            let date = new Date();
+            date.setDate(date.getDate() + 1);
+            document.cookie = 'MWpopup=close;expires=' + date.toUTCString();
+            todayPop.style.display = 'none'
+        };
+        if(document.cookie.match('close')){
+            todayPop.style.display = 'none'
+        }else{
+            todayPop.style.display = 'block'
+        }
+
+        $('.popup div button:nth-of-type(2)').click(function(){
+            $('.popup').fadeOut(200);
+        })
+
+
         // 헤더 버거메뉴
         const menuBtn = document.querySelector('.my a:nth-of-type(3)'), //버거버튼
         subMenu = document.querySelector('.submenu > div'), //버거 내용
@@ -34,22 +55,27 @@ function main(){
                 $(this).find('div').stop().slideDown(550);
                 if( $(this).children().length == 2){
                     $('.backwhite').addClass('white');
+                };
+                if( $('.submenu > div').hasClass('active')){
+                    $(this).find('div').stop().slideUp(150);
+                    $('.backwhite').removeClass('white');
                 }
             },
             mouseleave:function(){
                 $(this).find('div').stop().slideUp(150);
                 $('.backwhite').removeClass('white');
             }
+        
         })
 
-        // 헤더 미니 팝업
+        // 헤더 빨간 미니 팝업
         $('.minipop span').click(function(){
             $('.minipop').remove();
         });
 
         // 피자 목록
         let pzList='';
-        const pizza = document.querySelector('.pizza ul');
+        const pizzalist = document.querySelector('.pizza ul');
         data.pizza.forEach(function(v){
             pzList += `<li>
                             <figure>
@@ -60,7 +86,7 @@ function main(){
                             <article>
                                 <div>
                                     <span>L</span>
-                                    <span>${v.price}</span>
+                                    <span>${v.price}원</span>
                                     <span>${v.orig}</span>
                                 </div>
                                 <h2>배달25%</h2>
@@ -68,7 +94,7 @@ function main(){
                             </article>
                         </li>`
         })
-        pizza.innerHTML = pzList;
+        pizzalist.innerHTML = pzList;
 
 
         // 배너 슬라이더
@@ -81,30 +107,103 @@ function main(){
             vertical: true
         });
 
-        //장바구니 숫자
-        const cartNum = document.querySelector('.cart button'),
-            cartAddBtn = document.querySelectorAll('.pizza ul li article button');
-        let i=0, b=1;
+        //피자 + 누르면 장바구니 숫자
+        const cartNum = document.querySelector('.cart button'), //장바구니 위 버튼
+            cartAddBtn = document.querySelectorAll('.pizza ul li article button'), //피자리스트 더하기(+) 버튼
+            bkPizza = document.querySelector('.basket article'), //피자 글씨 & 큰사진
+            deliList = document.querySelector('.deliList ul'), // 주문내역
+            total = document.querySelector('.total'), //총금액
+            cloud = document.querySelector('.cloud'); //말풍선
+         let i=0, b=1, bkList='', dbList='';
+
         
         for(i=0; i<cartAddBtn.length; i++){
-            cartAddBtn[i].addEventListener('click',function(){
-                // 피자 등장
-                $('.order').hide(); 
-                $('.basket').fadeIn(500);
-                $('.basket h2 span').css({opacity:'1'}); //추천메뉴는~
-                $('.delivery').fadeIn(700);     //포장배달 주문하기
+            cartAddBtn[i].addEventListener('click',function(e){
+                $('.order').hide();             //기존 없애기
     
-                // 장바구니 담기
-                cartNum.style.display = 'block';
+                // 장바구니 숫자 담기
+                cartNum.style.display = 'block'
                 if(cartNum.textContent == '9'){
                     alert('피자는 최대 9판, 팬피자 최대 4판, 사이드 & 음료 최대 10개까지 가능합니다. 단체주문은 080-500-5588로 문의주시기 바랍니다.'); return;
                 }
                 cartNum.textContent = b++;
-            });
+                
+
+                
+                let bk = $(this).parents('li').index(); //인덱스 찾기
+
+                // 인덱스번호 맞춰서 해당 피자 내용
+                bkList += `<h2><span>내가 담은</span><span>${data.pizza[bk].name} L 1개</span></h2>
+                            <figure>
+                                <p><img src="img/bg_cart_empty_400.png" alt=""></p>
+                                <p class="plate"><img src="${data.pizza[bk].bgimg}" alt=""></p>
+                            </figure>`
+                bkPizza.innerHTML = bkList;
+
+                $('.basket').fadeIn(500);  // 피자 등장
+                $('.basket h2 > span').css({opacity:'1'}); // 상단 피자 이름 등장
+
+
+                $('.delivery').fadeIn(700);     //포장배달 주문하기 등장
+                // 클릭한 피자의 주문 내역 추가
+                dbList += `<li>
+                                <p>${data.pizza[bk].name}<b>(L)</b> 1개</p>
+                                <small>
+                                    <span>엣지</span>
+                                    <span>${data.pizza[bk].edge}</span>
+                                </small>
+                                <div>
+                                    <span>${data.pizza[bk].orig}원</span>
+                                    <span>${data.pizza[bk].sale}</span>
+                                    <span>${data.pizza[bk].price}원</span>
+                                </div>
+                            </li>`
+                deliList.innerHTML = dbList;
+                
+                if(deliList.children.length != 1){
+                    deliList.style.height = '190px'
+                }
+
+
+                // 합계 표시
+                let sum = [],
+                    priceEle = parseInt(data.pizza[bk].price.replace(/,/g , ''));
+                
+                sum.push(priceEle)
+                total.textContent = `(${bk + 1})` + sum + `원 주문`
+
+
+                let orderLen = $(bkPizza).children('figure').length;
+                bkPizza.style = `transform:translateY( ${-665 * (orderLen - 1)}px)`
+                cloud.style = `transform:translateY( ${-665 * (orderLen - 1)}px)`
+
+                // $(bkPizza).find('figure > p:nth-of-type(1)').css({'transform':'translateY(' + 665 * (orderLen - 1) + 'px)' });
+                // $(bkPizza).find('.plate').css({'transform':'translateY(' + -665 * (orderLen - 1) + 'px)' });
+                // $(bkPizza).find('h2').css({'transform':'translateY(' + -665 * (orderLen - 1) + 'px)' });
+                // $(cloud).css({'transform':'translateY(' + -665 * (orderLen - 1) + 'px)' });
+
+                console.log(bkPizza.hasChildNodes('figure'))
+
+
+            }); 
         }
+    
+        // 피자 목록 호버 높이 자연스럽게
+        let j=0;
+        const ulElement = document.querySelectorAll(".pizza ul li"),
+            initialHeight = getComputedStyle(ulElement[j]).height,
 
-
-
+            handlerMouseOver = (evt) => {
+            evt.currentTarget.style.height = evt.currentTarget.scrollHeight + 30 + "px";
+            },
+            handlerMouseOut = (evt) => {
+            evt.currentTarget.style.height = initialHeight;
+            };
+        
+        for(j=0; j<ulElement.length; j++){
+            ulElement[j].addEventListener("mouseenter", handlerMouseOver);
+            ulElement[j].addEventListener("mouseleave", handlerMouseOut);
+        }
         
 
     }
--- js/order.js
+++ js/order.js
@@ -7,9 +7,9 @@ function sub(){
 
         // 헤더 버거메뉴
         const menuBtn = document.querySelector('.my a:nth-of-type(3)'), //버거버튼
-        subMenu = document.querySelector('.submenu > div'), //버거 내용
-        subWhite = document.querySelector('.submenu > p'), //버거 흰배경
-        subBlack = document.querySelector('header'); //버거 검정배경
+            subMenu = document.querySelector('.submenu > div'), //버거 내용
+            subWhite = document.querySelector('.submenu > p'), //버거 흰배경
+            subBlack = document.querySelector('header'); //버거 검정배경
 
         menuBtn.addEventListener('click', function(){
             if(menuBtn.classList.contains('active')){
@@ -28,24 +28,51 @@ function sub(){
         });
 
 
-        //헤더 미니메뉴
-        $('.menu ul:nth-of-type(1) li').on({
+        // 헤더 슬라이드 메뉴
+        $('.menu ul li').on({
             mouseenter:function(){
                 $(this).find('div').stop().slideDown(550);
                 if( $(this).children().length == 2){
                     $('.backwhite').addClass('white');
+                };
+                if( $('.submenu > div').hasClass('active')){
+                    $(this).find('div').stop().slideUp(150);
+                    $('.backwhite').removeClass('white');
                 }
             },
             mouseleave:function(){
                 $(this).find('div').stop().slideUp(150);
                 $('.backwhite').removeClass('white');
             }
+        
         })
 
-        
-        let tpList='', sdList='', drList='', infoList='';
+        // 헤더 빨간 미니 팝업
+        $('.minipop span').click(function(){
+            $('.minipop').remove();
+        });
 
-        const topping = document.querySelector('.topping ul');
+
+        const topping = document.querySelector('.topping ul'),
+            side = document.querySelector('.side ul'),
+            drink = document.querySelector('.drink ul'),
+            sizeBtn = document.querySelectorAll('.size figure'),
+            qtMinus = document.querySelector('.quantity button:nth-of-type(1)'),
+            qtPlus = document.querySelector('.quantity button:nth-of-type(2)'),
+            qtNum = document.querySelector('.quantity div span');
+        let sizeNum = 0, tpList='', sdList='', drList='', infoList='', qtCount = 1;
+        
+        //사이즈 선택 버튼
+        for( let z=0; z<sizeBtn.length; z++){
+            sizeBtn[z].addEventListener('click',function(){
+                sizeBtn[sizeNum].classList.remove('active');
+                sizeBtn[z].classList.add('active');
+                sizeNum = z;
+                
+            })
+        }
+
+        // 토핑 목록
         data.topping.forEach(function(t){
             tpList += `<li class="tplist">
                             <p><img src="${t.img}"></p>
@@ -57,7 +84,43 @@ function sub(){
         topping.innerHTML = tpList;
 
 
-        const side = document.querySelector('.side ul');
+        const tpBtn = document.querySelectorAll('.tplist a');
+
+        for( let ck=0; ck<tpBtn.length; ck++){
+            tpBtn[ck].addEventListener('click',function(){
+                let target = event.target;
+                if( target.getAttribute('src') == 'img/chk_btn2.png'){
+                    target.src = `img/chk_btn1.png`; return;
+                }
+                target.src = `img/chk_btn2.png`
+            })
+        }
+
+        // 피자 수량 선택 - + 버튼
+        qtNum.textContent = `${qtCount}`
+
+        qtMinus.addEventListener('click',function(){
+            if( qtCount > 1 ){
+                qtCount--;
+                qtNum.textContent = `${qtCount}`;
+            }
+            qtNum.textContent = `${qtCount}`
+        })
+
+        qtPlus.addEventListener('click',function(){
+            if( qtCount < 10){ 
+                qtCount++
+                if( qtCount > 9){
+                    alert('피자는 최대 9판까지 가능합니다. 단체주문은 080-500-5588로 문의주시기 바랍니다.');
+                    qtCount--;
+                }
+                qtNum.textContent = `${qtCount}`
+               
+            }
+        })
+
+
+        // 사이드 목록
         data.side.forEach(function(s){
             sdList += `<li class="sdlist">
                             <figure>
@@ -77,8 +140,34 @@ function sub(){
         })
         side.innerHTML = sdList;
 
+        const sdMinus = document.querySelectorAll('.sdlist div button:nth-of-type(1)'),
+            sdPlus = document.querySelectorAll('.sdlist div button:nth-of-type(2)');
+        let sdCount = 0, sd = 0;
 
-        const drink = document.querySelector('.drink ul');
+        // sideNum.textContent = `${sdCount}`;
+
+        for(sd=0; sd<sdPlus.length; sd++ ){
+            sdPlus[sd].addEventListener('click',function(){
+                if(sdCount < 11){
+                    sdCount++;
+                    
+                    if( sdCount > 10){
+                        alert( 'ssss'); sdCount--;
+                    }
+                    $(this).siblings('span').text(sdCount)
+                }
+            })
+            sdMinus[sd].addEventListener('click',function(){
+                if(sdCount > 0){
+                    sdCount--;
+                    $(this).siblings('span').text(sdCount)
+                }
+            })
+            
+        }
+
+
+        // 소스&음료 목록
         data.drink.forEach(function(d){
             drList += `<li class="drlist">
                             <p><img src="${d.img}"></p>
@@ -91,13 +180,15 @@ function sub(){
         })
         drink.innerHTML = drList;
 
-
+        // 좌우 슬라이드
         $('.responsive').slick({
             dots: true, infinite: false,
             speed: 300, slidesToShow: 4, slidesToScroll: 4
         });
 
+
         const infoPop = document.querySelector('.facpop1 > div ul');
+        // 토핑정보 팝업
         data.tpinfo.forEach(function(i){
             infoList += `<li>
                             <p><img src="${i.img}"></p>
@@ -108,6 +199,55 @@ function sub(){
                         </li>`
         })
         infoPop.innerHTML = infoList;
+
+        $('.facts button:nth-of-type(1)').click(function(){
+            $('.facpop1').fadeIn(200)
+            $('.facpop1').find('button').click(function(){
+                $('.facpop1').fadeOut(100)
+            })
+        })
+
+        $('.facts button:nth-of-type(2)').click(function(){
+            $('.facpop2').fadeIn(200)
+            $('.facpop2').find('button').click(function(){
+                $('.facpop2').fadeOut(100)
+            })
+        })
+
+        $('.facts button:nth-of-type(3)').click(function(){
+            $('.facpop3').fadeIn(200)
+            $('.facpop3').find('button').click(function(){
+                $('.facpop3').fadeOut(100)
+            })
+        })
+
+        $('.facts button:nth-of-type(4)').click(function(){
+            $('.facpop4').fadeIn(200)
+            $('.facpop4').find('button').click(function(){
+                $('.facpop4').fadeOut(100)
+            })
+        })
+
+        
+
+
+
+
+
+
+        
+        // final 네비 스크롤 내리면 고정
+        const final = document.querySelector('.final'),
+            footer = document.querySelector('footer');
+
+        $(document).scroll(function() {
+            if( ( $(window).scrollTop() + $(window).innerHeight()) >= $(footer).offset().top ){
+                $(final).addClass('fixedOn')
+            }else{
+                $(final).removeClass('fixedOn')
+            }
+        });
+
     }
 };
 window.onload = sub;
